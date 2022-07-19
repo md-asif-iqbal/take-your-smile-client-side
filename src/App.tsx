78b0078b6aa684183eb9d13d2d5ad1a749f59dda
unknown <irfanulislamfahim@gmail.com>
2022-07-19 19:46:50 +0600

    four-events-banner-complete

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './Pages/shared/NavBar/NavBar';
+import Events from './Pages/Events/Events';
 
 function App() {
   return (
     <div className="relative bg-red">
-      <NavBar/>
+
+      <Events></Events>
     </div>
   );
 }
