09d16adfd1f0a05c33f4fe825ebcf5f1e6893b60
araf78 <alarafath71@gmail.com>
2022-07-19 12:21:14 +0600

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
+import Footer from './components/Footer';
 import logo from './logo.svg';
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> save to reload. yes
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Footer></Footer>
     </div>
   );
 }
