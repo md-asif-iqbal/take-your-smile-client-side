26877a6e3146e6c02653e8f34bd2a644ac41ace4
miqbal191034@bscse.uiu.ac.bd <asifiqbal@Asifs-MBP.Dlink>
2022-07-20 17:49:48 +0600

    Update tailwind data

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,8 @@ module.exports = {
     themes: [
       {
         light: {
-<<<<<<< HEAD
-          primary: "#FF014F",
-=======
+
           primary: "#FB4051",
->>>>>>> 28ab2cebb1ef48ccbe764b3b8434b7e1c0bf390c
           secondary: "#0D0D0D",
           neutral: "#ECF0F3",
           "base-100": "#ECF0F3",
@@ -21,8 +18,6 @@ module.exports = {
     ],
   },
   plugins: [require("daisyui")],
-<<<<<<< HEAD
+
 };
-=======
-};
->>>>>>> 28ab2cebb1ef48ccbe764b3b8434b7e1c0bf390c
+
