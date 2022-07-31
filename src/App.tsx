28873366281b0e3aa3a3f29e8d9c074b5e03af06
md-asif-iqbal <miqbal191034@bscse.uiu.ac.bd>
2022-07-31 23:35:28 +0600

    add

--- src/App.tsx
+++ src/App.tsx
@@ -41,9 +41,6 @@ import UploadEvents from './Pages/dashboard/UploadEvents';
 import Dashboard from './Pages/dashboard/Dashboard';
 import Blogs from './Pages/Blogs/Blogs';
 
-import {MessengerCustomerChat} from "typescript-react-facebook-messenger";
-import Messengar from './Pages/Messengar/Messengar';
-
 function App() {
 
   
@@ -100,7 +97,7 @@ function App() {
                <Route path='/singlegallery' element={<SingleEvent/>}></Route>
                <Route path='*' element={<Error/>}></Route>
              </Routes>
-             <Messengar/>
+       
             <Footer/>
             <ToastContainer />
             
