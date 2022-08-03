45f30b25834d357baabe35849f20b4805d7133a3
unknown <irfanulislamfahim@gmail.com>
2022-08-04 02:17:34 +0600

    like system complete

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import WeddingGallery from './Pages/EventGallery/WeddingGallery';
 import Events from './Pages/Events/Events';
 import HomeWithNav from './Pages/Home/Home/HomeWithNav';
 import Footer from './Pages/shared/Footer/Footer';
-import { ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Contact from './Pages/Contact/Contact';
 import Weedings from './Pages/Events/Weedings';
@@ -40,67 +40,69 @@ import Review from './Pages/dashboard/Review';
 import UploadEvents from './Pages/dashboard/UploadEvents';
 import Dashboard from './Pages/dashboard/Dashboard';
 import Blogs from './Pages/Blogs/Blogs';
+import Likes from './Pages/Likes/Posts';
+import Posts from './Pages/Likes/Posts';
 
 function App() {
 
-  
+
   return (
-      <div className="relative">
+    <div className="relative">
 
-             <Routes>
-               <Route path='/' element={<Events/>}></Route>
+      <Routes>
+        <Route path='/' element={<Events />}></Route>
 
 
-               <Route path='/home' element={<HomeWithNav/>}></Route>
-               <Route path='dashboard' element={<Dashboard></Dashboard>}>
-                 <Route path='allbooking' element={<AllBooking></AllBooking>}></Route>
-                 <Route path='booking' element={<Booking></Booking>}></Route>
+        <Route path='/home' element={<HomeWithNav />}></Route>
+        <Route path='dashboard' element={<Dashboard></Dashboard>}>
+          <Route path='allbooking' element={<AllBooking></AllBooking>}></Route>
+          <Route path='booking' element={<Booking></Booking>}></Route>
 
-                 {/* <Route path='profile' element={<UpdateProfile></UpdateProfile>}></Route> */}
-                 <Route path='reviews' element={<Review></Review>}></Route>
-                 <Route path='users' element={<Users></Users>}></Route>
-                 <Route path='uploadEvents' element={<UploadEvents/>}></Route>
+          {/* <Route path='profile' element={<UpdateProfile></UpdateProfile>}></Route> */}
+          <Route path='reviews' element={<Review></Review>}></Route>
+          <Route path='users' element={<Users></Users>}></Route>
+          <Route path='uploadEvents' element={<UploadEvents />}></Route>
 
-                 <Route path='admin' element={<MakeAdmin></MakeAdmin>}></Route>
-                 <Route path='profile/update' element={<Update />} />
-                 <Route index element={<Profile/>} />
-               </Route>
-      
-               {/* <NavBar/> */}
-               
-               <Route path='/blogs' element={<Blogs></Blogs>}></Route>
-               <Route path='/blogsone' element={<BlogsOne></BlogsOne>}></Route>
-               <Route path='/blogsthree' element={<BlogsThree></BlogsThree>}></Route>
-              
-               <Route path='/home' element={<HomeWithNav />}></Route>
-               <Route path='/ourstory' element={<OurStory />}></Route>
-               <Route path='/weddings' element={<Weedings />}></Route>
-               <Route path='/corporate' element={<Corporate />}></Route>
-               <Route path='/social' element={<Social />}></Route>
-               <Route path='/nonprofit' element={<NonProfit />}></Route>
-               <Route path='/contactus' element={<RequireAuth>
-                  <Contact />
-               </RequireAuth>
-               }></Route>
-               <Route path='/login' element={<Login />}></Route>
-                
-               <Route path='/gallery' element={<EventGallery/>}>
-                <Route index element={<AllGallery/>}></Route>
-                 <Route path='socialgallery' element={<SocialGallery/>}></Route>
-                 <Route path='nonprofitgallery' element={<NonProfitGallery/>}></Route>
-                 <Route path='weddinggallery' element={<WeddingGallery/>}></Route>
-                 <Route path='corporategallery' element={<CorporateGallery/>}></Route>
-               </Route>
+          <Route path='admin' element={<MakeAdmin></MakeAdmin>}></Route>
+          <Route path='profile/update' element={<Update />} />
+          <Route index element={<Profile />} />
+        </Route>
 
-              
+        {/* <NavBar/> */}
 
-               <Route path='/singlegallery' element={<SingleEvent/>}></Route>
-               <Route path='*' element={<Error/>}></Route>
-             </Routes>
-       
-            <Footer/>
-            <ToastContainer />
-            
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/blogsone' element={<BlogsOne></BlogsOne>}></Route>
+        <Route path='/blogsthree' element={<BlogsThree></BlogsThree>}></Route>
+
+        <Route path='/home' element={<HomeWithNav />}></Route>
+        <Route path='/ourstory' element={<OurStory />}></Route>
+        <Route path='/weddings' element={<Weedings />}></Route>
+        <Route path='/corporate' element={<Corporate />}></Route>
+        <Route path='/social' element={<Social />}></Route>
+        <Route path='/nonprofit' element={<NonProfit />}></Route>
+        <Route path='/contactus' element={<RequireAuth>
+          <Contact />
+        </RequireAuth>
+        }></Route>
+        <Route path='/login' element={<Login />}></Route>
+
+        <Route path='/gallery' element={<EventGallery />}>
+          <Route index element={<AllGallery />}></Route>
+          <Route path='socialgallery' element={<SocialGallery />}></Route>
+          <Route path='nonprofitgallery' element={<NonProfitGallery />}></Route>
+          <Route path='weddinggallery' element={<WeddingGallery />}></Route>
+          <Route path='corporategallery' element={<CorporateGallery />}></Route>
+        </Route>
+
+        <Route path='/likes' element={<Posts></Posts>}></Route>
+
+        <Route path='/singlegallery' element={<SingleEvent />}></Route>
+        <Route path='*' element={<Error />}></Route>
+      </Routes>
+
+      <Footer />
+      <ToastContainer />
+
 
     </div>
   );
