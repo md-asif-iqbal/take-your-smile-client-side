ee36a85f30117734264b7c5a27c9c44e1a2a47eb
md-asif-iqbal <miqbal191034@bscse.uiu.ac.bd>
2022-08-09 12:23:47 +0600

    marge

--- src/Pages/EventGallery/AllGallery.tsx
+++ src/Pages/EventGallery/AllGallery.tsx
@@ -2,14 +2,26 @@ import React from 'react';
 import PageTitle from '../shared/PageTitle/PageTitle';
 import { properties } from './constants/data';
 import Card from './Card';
+import Loading from '../shared/Loading/Loading';
+import { useQuery } from '@tanstack/react-query';
 
 const AllGallery = () => {
+  const url = `http://localhost:8000/gallerys`;
+  const { data: gallerys, isLoading , refetch } = useQuery(['allparts'], () => fetch(url, {
+    method: 'GET',
+    headers: {
+        'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+    }
+}).then(res => res.json()));
+if(isLoading){
+    return <Loading></Loading>
+}
     return (
         <section>
           <PageTitle title="All Gallery" />
           <div className="Appes">
       <div className="propertiest">
-        {properties.map((item) => (
+        {gallerys.map((item) => (
           <Card data={item} key={item.date} />
         ))}
       </div>
