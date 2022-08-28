7401259c73aa833b1b2476bf227716cc53db75f1
araf78 <alarafath71@gmail.com>
2022-08-28 12:02:28 +0600

    Scroll to Top

--- src/Pages/ScrollTop/ClickToTop.tsx
+++ src/Pages/ScrollTop/ClickToTop.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ScrollToTop from 'react-scroll-to-top';
-import './t.css'
+import ScrollTop from './ScrollTop';
+
 const ClickToTop = () => {
     
     return (
-        <div>
-			<ScrollToTop smooth top="1000" />
+        <div className='radius-20 bg-inherit text-green-900'>
+			<ScrollTop></ScrollTop>
            
         </div>
     );
